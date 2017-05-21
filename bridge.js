806a2758589934f3a91981cb73436b851164ffe1
Mark K Cowan <mark@battlesnake.co.uk>
2017-05-21 03:08:08 +0100

    bridge: now runs without requiring radio SW to be connected

--- bridge.js
+++ bridge.js
@@ -1,6 +1,12 @@
 #!/usr/bin/env node
 
+const gpredict_default_port = 6969;
+
+const default_ref_freq = 145.890e6;
+
 const RegRPC = require('./regrpc');
+const net = require('net');
+const random_id = require('random').chars('0123456789abcdef');
 
 const gs = process.env.GS || process.argv[2];
 
@@ -9,25 +15,16 @@ if (!gs) {
 	process.exit(1);
 }
 
-const net = require('net');
-
-const gpredict_default_port = 6969;
-
 const gpredict = { host: '127.0.0.1', port: process.env.PORT || gpredict_default_port };
-
-const random_id = require('random').chars('0123456789abcdef');
 
 const _regs = RegRPC.create({ name: `gp:${process.env.INSTANCE || random_id(8)}` });
 
-const ref_freq = 145.89e6;
+const ref_freq = +(process.env.REF_FREQ || default_ref_freq);
 
-const session = (regs, sock, rx_freq, tx_freq) => {
+const session = (regs, sock) => {
 
 	let RX = false;
 	let TX = false;
-
-	let drx = 0;
-	let dtx = 0;
 
 	const handle_cmd = async data => {
 		/*
@@ -48,19 +45,6 @@ const session = (regs, sock, rx_freq, tx_freq) => {
 			RX = false;
 			TX = true;
 		}
-		const write = async (fn, def) => {
-			let value = def;
-			let e;
-			try {
-				value = await fn();
-			} catch (err) {
-				e = err;
-			}
-			sock.write(String(value));
-			if (e) {
-				throw e;
-			}
-		};
 		if (/^[a-z]/.test(data)) {
 			/* gPredict getters dummmy response */
 			if (/t/.test(data) && RX) {
@@ -68,11 +52,9 @@ const session = (regs, sock, rx_freq, tx_freq) => {
 			} else if (/t/.test(data) && TX) {
 				sock.write('1');
 			} else if (/f/.test(data) && RX) {
-				const shift = await regs.get('RX frequency shift');
-				await write(() => ref_freq * shift / rx_freq, ref_freq);
+				sock.write(String(0));
 			} else if (/f/.test(data) && TX) {
-				const shift = await regs.get('TX frequency shift');
-				await write(() => ref_freq * shift / tx_freq, ref_freq);
+				sock.write(String(0));
 			}
 		} else if (/^[A-Z]/.test(data)) {
 			/* gPredict setters */
@@ -80,13 +62,9 @@ const session = (regs, sock, rx_freq, tx_freq) => {
 				if (/^F/.test(data)) {
 					const value = data.match(/\d+/)[0];
 					if (RX) {
-						const shift = (value / ref_freq - 1) * rx_freq;
-						await regs.set('RX frequency shift', shift);
-						drx = shift;
+						await regs.set('RX relative frequency shift', value / ref_freq - 1);
 					} else if (TX) {
-						const shift = (value / ref_freq - 1) * tx_freq;
-						await regs.set('TX frequency shift', shift);
-						dtx = shift;
+						await regs.set('TX relative frequency shift', value / ref_freq - 1);
 					}
 				}
 			} finally {
@@ -100,27 +78,13 @@ const session = (regs, sock, rx_freq, tx_freq) => {
 		try {
 			await handle_cmd(rawdata.toString('utf8'));
 		} catch (e) {
-			console.error(`Failed to execute command: ${e.stack}`);
-			sock.destroy();
+			/* Null */
 		}
 	};
 
 	console.log(`gPredict connection from ${sock.remoteAddress}:${sock.remotePort}`);
 
-	const dashboard = () => {
-		let s = `f_rx=${(rx_freq + drx) | 0}, f_tx=${(tx_freq + dtx) | 0}, Δf_rx=${drx | 0} Δf_tx=${dtx | 0}`;
-		while (s.length < process.env.COLUMNS) {
-			s += ' ';
-		}
-		process.stderr.write(`${s}\r`);
-	};
-
-	const interval = setInterval(dashboard, 300);
-
-	const closed = () => {
-		clearInterval(interval);
-		console.log(`gPredict conncetion closed from ${sock.remoteAddress}:${sock.remotePort}`);
-	};
+	const closed = () => console.log(`gPredict conncetion closed from ${sock.remoteAddress}:${sock.remotePort}`);
 
 	sock.on('data', handle_cmd_wrap);
 
@@ -129,26 +93,13 @@ const session = (regs, sock, rx_freq, tx_freq) => {
 	process.stderr.write('\n');
 };
 
-async function get_current_freq(regs) {
-	try {
-		return await Promise.all([regs.get('RX frequency'), regs.get('TX frequency')]);
-	} catch (e) {
-		console.error('Failed to query radio');
-		throw e;
-	}
-}
-
 async function run() {
 	const regs = (await _regs).bind(gs);
 	console.info('');
 	console.info(`TUNE gPredict RX+TX FREQUENCIES TO ${(ref_freq / 1e6).toFixed(6)}MHz!`);
+	const server = net.createServer(socket => session(regs, socket));
+	await new Promise((res, rej) => server.listen(gpredict.port, gpredict.host, 1, e => e ? rej(e) : res()));
 	console.info('');
-	console.info('Querying radio centre frequency');
-	const [rx, tx] = (await get_current_freq(regs)).map(Number);
-	console.info(`Query resolved, radio is configured for rx=${rx} tx=${tx}`);
-	console.info('');
-	const server = net.createServer(socket => session(regs, socket, rx, tx));
-	server.listen(gpredict.port, gpredict.host);
 	console.log(`Server listening on ${gpredict.host}:${gpredict.port}`);
 	console.info('');
 }
